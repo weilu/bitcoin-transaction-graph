da0360ac2388160c8be12090c51043a5a6044205
Wei Lu <luwei.here@gmail.com>
2014-08-26 12:43:37 +0800

    node.addToNextNodes & node.removeFromNextNodes

--- node.js
+++ node.js
@@ -5,4 +5,34 @@ function Node(id, tx, prevNodes, nextNodes) {
   this.nextNodes = nextNodes || []
 }
 
+Node.prototype.addToNextNodes = function(toAdd)  {
+  if(this.nextNodes.indexOf(toAdd) >= 0) {
+    console.warn(this.id, 'already contain', toAdd.id, "in nextNodes")
+  } else {
+    this.nextNodes.push(toAdd)
+    if(toAdd.prevNodes.indexOf(this) >= 0) {
+      console.warn(toAdd.id, 'already contain', this.id, "in prevNodes")
+    } else {
+      toAdd.prevNodes.push(this)
+    }
+  }
+}
+
+Node.prototype.removeFromNextNodes = function(toRemove) {
+  var index = this.nextNodes.indexOf(toRemove)
+  if(index >= 0) {
+    this.nextNodes.splice(index, 1)
+
+    var prevNodeIndex = toRemove.prevNodes.indexOf(this)
+    if(prevNodeIndex >= 0) {
+      toRemove.prevNodes.splice(prevNodeIndex, 1)
+    } else {
+      console.warn(toRemove.id, 'does not contain', this.id, "in prevNodes")
+    }
+  } else {
+    console.warn(this.id, 'does not contain', toRemove.id, "in nextNodes")
+  }
+}
+
+
 module.exports = Node
--- test/node.js
+++ test/node.js
@@ -17,4 +17,52 @@ describe('Node', function() {
       assert.deepEqual(node.nextNodes, [])
     })
   })
+
+  describe('addToNextNodes', function() {
+    var node1, node2
+
+    beforeEach(function() {
+      node1 = new Node(1)
+      node2 = new Node(2)
+    })
+
+    it('works and takes care of prevNodes relationship', function() {
+      node1.addToNextNodes(node2)
+      assert(node1.nextNodes.indexOf(node2) >= 0)
+      assert(node2.prevNodes.indexOf(node1) >= 0)
+    })
+
+    it('does not double add', function() {
+      node1.addToNextNodes(node2)
+      var nextNodesLength = node1.nextNodes.length
+      var prevNodesLength = node2.prevNodes.length
+
+      node1.addToNextNodes(node2)
+
+      assert.equal(node1.nextNodes.length, nextNodesLength)
+      assert.equal(node2.prevNodes.length, prevNodesLength)
+    })
+  })
+
+  describe('removeFromNextNodes', function() {
+    var node1, node2
+
+    beforeEach(function() {
+      node1 = new Node(1)
+      node2 = new Node(2)
+    })
+
+    it('works and takes care of prevNodes relationship', function() {
+      node1.addToNextNodes(node2)
+      node1.removeFromNextNodes(node2)
+      assert(node1.nextNodes.indexOf(node2) < 0)
+      assert(node2.prevNodes.indexOf(node1) < 0)
+    })
+
+    it('does not error when the target node does not exist in nextNodes', function() {
+      assert.doesNotThrow(function() {
+        node1.removeFromNextNodes(node2)
+      })
+    })
+  })
 })
