63bb020fc6aa941296dc542664abcd1bebfff300
Wei Lu <luwei.here@gmail.com>
2014-08-31 14:42:51 +0800

    Remove node.addToNextNodes and node.removeFromNextNodes

--- node.js
+++ node.js
@@ -5,30 +5,4 @@ function Node(id, tx, prevNodes, nextNodes) {
   this.nextNodes = nextNodes || []
 }
 
-Node.prototype.addToNextNodes = function(toAdd)  {
-  if(this.nextNodes.indexOf(toAdd) >= 0 || toAdd === this) return;
-
-  this.nextNodes.push(toAdd)
-  if(toAdd.prevNodes.indexOf(this) >= 0) {
-    console.warn(toAdd.id, 'already contain', this.id, "in prevNodes")
-  } else {
-    toAdd.prevNodes.push(this)
-  }
-}
-
-Node.prototype.removeFromNextNodes = function(toRemove) {
-  var index = this.nextNodes.indexOf(toRemove)
-  if(index < 0) return;
-
-  this.nextNodes.splice(index, 1)
-
-  var prevNodeIndex = toRemove.prevNodes.indexOf(this)
-  if(prevNodeIndex >= 0) {
-    toRemove.prevNodes.splice(prevNodeIndex, 1)
-  } else {
-    console.warn(toRemove.id, 'does not contain', this.id, "in prevNodes")
-  }
-}
-
-
 module.exports = Node
